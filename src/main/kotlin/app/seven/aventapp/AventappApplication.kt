9d83db075b51f1e9cf784057855e070fff59a358
Oga-Emma <35017091+Oga-Emma@users.noreply.github.com>
2024-02-12 01:27:07 +0100

    Initial commit

---
+++ src/main/kotlin/app/seven/aventapp/AventappApplication.kt
@@ -0,0 +1,11 @@
+package app.seven.aventapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AventappApplication
+
+fun main(args: Array<String>) {
+	runApplication<AventappApplication>(*args)
+}
---
+++ src/test/kotlin/app/seven/aventapp/AventappApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.seven.aventapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AventappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
